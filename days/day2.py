538c28c50e6afdf5d451f5d197d6026f850f4d4c
LauraZ <zaharialaura9@gmail.com>
2022-12-02 15:28:40 +0000

    Add solution for day 2

--- days/day2.py
+++ days/day2.py
@@ -1,7 +1,80 @@
 """ Day 2: Rock Paper Scissors """
 
-def main():
-  pass
+from utils.utils import *
+
+def main(game=None):
+  if not game:
+    game = read_data('input/input_day2.txt')
+  game = clean_data(game)
+
+  # when second column is my move
+  total_score = 0
+  for round in game:
+    total_score += move_score(round) + game_score(round)
+  print("Total score when second column is my move: ", total_score)
+
+  # when second column is the output of the game
+  total_score_v2 = 0
+  for round in game:
+    total_score_v2 += game_score_v2(round)
+  print("The score when the second column is the output of the game: ", total_score_v2)
+  
+  return total_score, total_score_v2
+
+
+# measure the score for your move
+def move_score(round):
+  my_move = round[-1]
+  score = 0
+
+  if my_move == 'X':
+    score = 1
+  elif my_move == 'Y':
+    score = 2
+  else:
+    score = 3
+
+  return score
+
+
+# measure the score for the game
+def game_score(round):
+  loses = ['B X', 'C Y', 'A Z']
+  draw = ['A X', 'B Y', 'C Z']
+  wins = ['C X', 'A Y', 'B Z']
+
+  score = 0
+  if round in loses:
+    score = 0
+  elif round in draw:
+    score = 3
+  elif round in wins:
+    score = 6
+
+  return score
+
+
+# column two is the game score
+def game_score_v2(round):
+  loses = ['B X', 'C Y', 'A Z']
+  draw = ['A X', 'B Y', 'C Z']
+  wins = ['C X', 'A Y', 'B Z']
+
+  output = round[-1]
+  if output == 'X':
+    for element in loses:
+      if element[0] == round[0]:
+        score = move_score(element[-1])
+  elif output == 'Y':
+    for element in draw:
+      if element[0] == round[0]:
+        score = move_score(element[-1]) + 3
+  elif output == 'Z':
+    for element in wins:
+      if element[0] == round[0]:
+        score = move_score(element[-1]) + 6
+  return score
+
 
 if __name__ == "__main__":
   main()
--- tests/day2_test.py
+++ tests/day2_test.py
@@ -1,4 +1,31 @@
 from days.day2 import *
 
 def test_main():
-  assert main() == None
+  input = ['A Y\n', "B X\n", "C Z\n"]
+  output = 15, 12
+  assert main(input) == output
+
+def test_move_score():
+  assert move_score('A X') == 1
+  assert move_score('B Y') == 2
+  assert move_score('A Z') == 3
+
+def test_game_score_lose():
+  assert game_score('B X') == 0
+  assert game_score('C Y') == 0
+  assert game_score('A Z') == 0
+
+def test_game_score_draw():
+  assert game_score('A X') == 3
+  assert game_score('B Y') == 3
+  assert game_score('C Z') == 3
+
+def test_game_score_lose():
+  assert game_score('C X') == 6
+  assert game_score('A Y') == 6
+  assert game_score('B Z') == 6
+
+def test_game_score_v2():
+  assert game_score_v2('A Y') == 4
+  assert game_score_v2('B X') == 1
+  assert game_score_v2('C Z') == 7
